5386768eeba4e2c80dfe11422e7a2a229a94ba25
Khổng Tám <22690521.tam@student.iuh.edu.vn>
2026-05-30 18:18:01 +0700

    fix upload

--- backend/services/company-service/src/main/java/iuh/fit/companyservice/controller/UploadController.java
+++ backend/services/company-service/src/main/java/iuh/fit/companyservice/controller/UploadController.java
@@ -1,44 +1,37 @@
 package iuh.fit.companyservice.controller;
 
-import iuh.fit.companyservice.Service.S3Service;
+import iuh.fit.companyservice.client.StorageClient;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.*;
-
-import java.util.Map;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/api/v1/company/upload")
 public class UploadController {
-    private final S3Service s3Service;
+    private final StorageClient storageClient;
 
-    public UploadController(S3Service s3Service) {
-        this.s3Service = s3Service;
+    public UploadController(StorageClient storageClient) {
+        this.storageClient = storageClient;
     }
+
     @GetMapping("/presigned-url")
     public ResponseEntity<?> getPresignedUrl(
             @RequestParam("key") String key,
             @RequestParam("contentType") String contentType
     ) {
         try {
-            System.out.println("KEY = " + key);
-            System.out.println("TYPE = " + contentType);
-
-            String url = s3Service.generatePresignedUrl(key, contentType);
-            String fileUrl = s3Service.getFileUrl(key);
-
-            return ResponseEntity.ok(Map.of(
-                    "uploadUrl", url,
-                    "fileUrl", fileUrl
-            ));
+            return ResponseEntity.ok(storageClient.getPresignedUrl(key, contentType));
         } catch (Exception e) {
             e.printStackTrace();
             return ResponseEntity.status(500).body(e.getMessage());
         }
-
     }
+
     @DeleteMapping("/delete")
     public ResponseEntity<?> deleteFile(@RequestParam("key") String key) {
-        s3Service.deleteFile(key);
-        return ResponseEntity.ok("Deleted");
+        return ResponseEntity.ok(storageClient.deleteFile(key));
     }
 }
