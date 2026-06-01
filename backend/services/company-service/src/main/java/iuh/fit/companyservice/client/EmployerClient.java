fbdc05c92a807313914be5ca9d1dd9d7b7bad151
Khổng Tám <22690521.tam@student.iuh.edu.vn>
2026-06-01 14:29:32 +0700

    update

--- backend/services/company-service/src/main/java/iuh/fit/companyservice/client/EmployerClient.java
+++ backend/services/company-service/src/main/java/iuh/fit/companyservice/client/EmployerClient.java
@@ -6,7 +6,7 @@ import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 
-@FeignClient(name = "user-service",url = "http://localhost:8083")
+@FeignClient(name = "user-service")
 public interface EmployerClient {
     @PostMapping("/api/v1/user/employer/save")
     EmployerResponse saveEmployerCompany(@RequestBody EmployerCompanyRequest request);
--- backend/services/payment-service/src/main/java/iuh/fit/paymentservice/Client/CompanyClient.java
+++ backend/services/payment-service/src/main/java/iuh/fit/paymentservice/Client/CompanyClient.java
@@ -5,7 +5,7 @@ import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 
-@FeignClient(name = "company-service",url = "http://localhost:8081")
+@FeignClient(name = "company-service")
 public interface CompanyClient {
     @PostMapping("/api/v1/company/subscription")
 void saveCompanySubscription(@RequestBody CompanySubscriptionRequest companySubscription);
